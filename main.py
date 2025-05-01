1a0bff108438388b379db3d20b80f02be9968e86
LeafEvans <leafevans@foxmail.com>
2025-05-01 19:16:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
 model = YOLO("yolo12n-tta-sense.pt")
-for _ in model("sample.mp4", save=True, tta=True, tta_bn_only=False, stream=True):
+for _ in model("sample.mp4", save=True, tta=False, stream=True):
     pass
