a95d00733ab9f2e8d895111493949e9a4ff15ffa
LeafEvans <leafevans@foxmail.com>
2025-05-09 11:33:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo12n-tta-sense.pt")
+model = YOLO("yolo11n.pt")
 
-for _ in model("sample.mp4", tta=False, tta_update_mode="adaptor", tta_loss_mode="unweighted_obj", stream=True):
-    pass
+model.train(data="coco128.yaml")
